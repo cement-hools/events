8607d2a8f830ca0c9f09cbb5234ab0a2debf61d9
Максим Секачёв <cement-fan@ya.ru>
2021-02-21 17:11:57 +0300

    static3

--- Moshikov/urls.py
+++ Moshikov/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from Moshikov import settings
 
 urlpatterns = [
 
@@ -30,3 +33,5 @@ urlpatterns = [
 
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
